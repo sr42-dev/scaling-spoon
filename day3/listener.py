f9c8ac9ebc65166cf45a3695dd57b952fda6e06a
Samuel Thomas <samuelthomas1049@gmail.com>
2022-11-08 16:13:36 +0530

    Added updated listener, talker and turtlesim move

--- day3/listener.py
+++ day3/listener.py
@@ -1,19 +1,24 @@
+#!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
 
 def chatter_callback(message):
-    rospy.init_node('listener', anonymous = True)
+    #get_caller_id(): Get fully resolved name of local node
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", message.data)
+    
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # node are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber("chatter", String, chatter_callback)
 
-    print("I heard %s", message.data)
-
+    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        chatter_callback()
-    except rospy.ROSInterruptException:
-        pass
-
-
+    listener()
